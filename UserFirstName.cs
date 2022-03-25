87b80d521f437ab96af5cf30e31b0e78e83a079c
RISHABH <gguptarishabh82@gmail.com>
2022-03-26 01:36:47 +0530

    First name starts with Cap and has minimum 3 characters

---
+++ UserFirstName.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Day19_User_Reg_Problem
+{
+    internal class UserFirstName
+    {
+        public static string FirstName = "^[A-Z]{1}[A-Z a-z]{6}$";
+        public bool Validate_First_Name(string First_Name)
+        {
+            return Regex.IsMatch(First_Name, FirstName);
+        }
+    }
+}
