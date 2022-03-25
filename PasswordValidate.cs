e9a67cf881e953dd1d83560171e4ba5d1673c590
RISHABH <gguptarishabh82@gmail.com>
2022-03-26 02:08:19 +0530

    Follow pre-defined Password rules.Rule1– minimum 8 Characters

---
+++ PasswordValidate.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Day19_User_Reg_Problem
+{
+    public class PasswordValidate
+    {
+        public string RegexPattern = "^[a-zA-Z0-9]{8,}$";
+        public bool passwordCheck(string password)
+        {
+            return Regex.IsMatch(password, RegexPattern);
+        }
+    }
+}
