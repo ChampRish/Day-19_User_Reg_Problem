63d7aeb5094b13a40bd0cc6a8a6ea571034297d9
RISHABH <gguptarishabh82@gmail.com>
2022-03-26 02:20:43 +0530

    Rule4–Pass Has exactly 1 Special Character

---
+++ SpecialCharacterValidate.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Day19_User_Reg_Problem
+{
+    public class SpecialCharacterValidate
+    {
+        public string RegexPattern = "^[a-zA-Z0-9]{1,}[@_?&*;:!$]{1}[a-zA-Z0-9]{1,}$";
+        public bool SpecialCharCheck(string specialvalidate)
+        {
+            return Regex.IsMatch(specialvalidate, RegexPattern);
+        }
+    }
+}
