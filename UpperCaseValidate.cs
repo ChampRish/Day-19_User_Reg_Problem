c5f7de2eabea38080d259b0af1fd7ecc30147b9c
Rishabh gupta <99132919+ChampRish@users.noreply.github.com>
2022-03-28 20:56:25 +0530

    Rule2  – Pass Should have at least 1 Upper Case

--- UpperCaseValidate.cs
+++ UpperCaseValidate.cs
@@ -8,5 +8,10 @@ namespace Day19_User_Reg_Problem
 {
     internal class UpperCaseValidate
     {
+        public string RegexPattern = "^[A-Z]{1}[a-zA-Z0-9]{7,}$";
+        public bool UpperCaseCheck(string UCase)
+        {
+            return Regex.IsMatch(UCase, RegexPattern);
+        }
     }
 }
